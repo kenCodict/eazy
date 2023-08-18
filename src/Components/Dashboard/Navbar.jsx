4295ddb22a6a34c5c71947f2e98d5351a9a29d0c
Ezeora Kenechukwu Johnbosco <kencodict@gmail.com>
2023-08-18 17:27:10 +0100

    little changes

--- src/Components/Dashboard/Navbar.jsx
+++ src/Components/Dashboard/Navbar.jsx
@@ -9,7 +9,6 @@ import rounded from '../../assets/rounded.svg'
 import bgtest from '../../assets/bgtest.png'
 import logo from '../../assets/logo.png'
 import advert from '../../assets/adver.png'
-
 import dash from '../../assets/dashSample.svg'
 import bg from '../../assets/phonebg.svg'
 import phone from '../../assets/phone.svg'
